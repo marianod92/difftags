b29ea724c15b5b53440c88a20dc9861128f95efe
marianod92 <mariano.dimodugno@gmail.com>
2022-05-08 23:31:49 -0300

    Update working dir repo

--- parser.py
+++ parser.py
@@ -1,19 +1,18 @@
 from pydriller import Repository
 import re
 
-for commit in Repository('https://github.com/marianod92/difftags',from_tag='v0.1',to_tag='v0.5').traverse_commits():
-    # print(commit.hash)
+# for commit in Repository('https://github.com/marianod92/difftags',from_tag='v0.1',to_tag='v0.5').traverse_commits():
+for commit in Repository('.',from_tag='v0.1',to_tag='v0.5').traverse_commits():
 
+    # print(commit.hash)
     print("Message: ", commit.msg)
 
     regex = r"[rR][bB][\s-]+[0-9]{4}"
-
 
     if re.search(regex, commit.msg):
         print("SI", "Autor: ", commit.author.name, " - " , "Message: ", commit.msg)
     # else:
         # print("NO", "Autor: ", commit.author.name, " - " , "Message: ", commit.msg)
-
 
     # for file in commit.modified_files:
     #     print(file.filename, ' has changed')
